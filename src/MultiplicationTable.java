44d71321e1fc37d33852c79bd5c152a712afbfee
Nasseima <lindsaynasseima@gmail.com>
2024-07-03 10:29:45 -0400

    Finally found it

--- src/MultiplicationTable.java
+++ src/MultiplicationTable.java
@@ -2,7 +2,7 @@ public class MultiplicationTable {
     public static void main(String[] args) {
 
         for (int i = 1; i <= 12; i++) {
-            for (int j = 1; j <= i; j++) {
+            for (int j = 1; j <= 12; j++) {
                 int sum = i * j;
                 System.out.println( i + " * " +  j + " = " + sum );
 
