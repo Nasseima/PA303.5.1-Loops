e21bdc686da6151fe434cd1b484acca4d390de65
Nasseima <lindsaynasseima@gmail.com>
2024-07-03 12:28:20 -0400

    I'm having a few issues with the last question

--- src/PredictFutureTuition.java
+++ src/PredictFutureTuition.java
@@ -1,2 +1,17 @@
 public class PredictFutureTuition {
+    public static void main (String [] args) {
+
+        //int tuition = 10000;
+//        int year = 0;
+
+        for (int tuition = 10000; tuition > 10000; tuition++ ) {
+            for (int year = 0; year > 0; year++);
+//            i += 7;
+//            tuition += i;
+//            System.out.println("");
+
+
+
+        }
+    }
 }
